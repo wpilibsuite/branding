ee476323500684f39f15031c4ea69dcfb8251595
Tyler Veness <calcmogul@gmail.com>
2017-03-08 21:36:48 -0800

    Added Inkscape SVG source files and created alternate logos
    wpilib-horizontal.svg places text to the right of the icon. wpilib-vertical.svg places it below. wpilib-horizontal.svg was added to the top of README.md.

--- cleanup-svg.sh
+++ cleanup-svg.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-# Removes cruft Inkscape adds to plain SVG files
-sed -e '/<sodipodi:namedview/,/>/d' \
-    -e '/sodipodi/d' \
-    -e '/inkscape/d' \
-    -e 's/id="svg8"$/id="svg8">/' \
+sed -e '/sodipodi:docname/d' \
+    -e '/inkscape:export/d' \
+    -e '/inkscape:version/d' \
+    -e '/<metadata/,/<\/metadata>/d' \
+    -e '/<defs/,/>/d' \
+    -e '/<sodipodi:namedview/,/>/d' \
+    -i wpilib-edittable.svg
+# Also add ">" after svg tag's width attribute
+
+sed -e '/<metadata/,/<\/metadata>/d' \
+    -e '/<defs/,/>/d' \
     -i wpilib.svg
+
+sed -e '/sodipodi:docname/d' \
+    -e '/inkscape:export/d' \
+    -e '/inkscape:version/d' \
+    -e '/<metadata/,/<\/metadata>/d' \
+    -e '/<defs/,/>/d' \
+    -e '/<sodipodi:namedview/,/>/d' \
+    -i wpilib-edittable-horizontal.svg
+# Also add ">" after svg tag's width attribute
+
+sed -e '/<metadata/,/<\/metadata>/d' \
+    -e '/<defs/,/>/d' \
+    -i wpilib-horizontal.svg
+
+sed -e '/sodipodi:docname/d' \
+    -e '/inkscape:export/d' \
+    -e '/inkscape:version/d' \
+    -e '/<metadata/,/<\/metadata>/d' \
+    -e '/<defs/,/>/d' \
+    -e '/<sodipodi:namedview/,/>/d' \
+    -i wpilib-edittable-vertical.svg
+# Also add ">" after svg tag's width attribute
+
+sed -e '/<metadata/,/<\/metadata>/d' \
+    -e '/<defs/,/>/d' \
+    -i wpilib-vertical.svg
