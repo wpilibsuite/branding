475fb5955fe3a7664f2acf58ed964b8b2f3cc51e
Tyler Veness <calcmogul@gmail.com>
2023-12-04 15:12:11 -0800

    Use black for Python formatting (#10)

--- export.py
+++ export.py
@@ -7,9 +7,7 @@ import sys
 
 
 def main():
-    svg = [
-        f for f in os.listdir(".") if os.path.isfile(f) and f.endswith(".svg")
-    ]
+    svg = [f for f in os.listdir(".") if os.path.isfile(f) and f.endswith(".svg")]
 
     # Generate plain SVG
     print("Generating plain SVGs from Inkscape SVGs...")
@@ -21,20 +19,25 @@ def main():
     for i in range(len(svg)):
         print(plain_svg[i] + "...", end="")
         sys.stdout.flush()
-        subprocess.run([
-            "inkscape", "-z", "--file=" + svg[i],
-            "--export-plain-svg=" + plain_svg[i], "--export-text-to-path"
-        ])
+        subprocess.run(
+            [
+                "inkscape",
+                "-z",
+                "--file=" + svg[i],
+                "--export-plain-svg=" + plain_svg[i],
+                "--export-text-to-path",
+            ]
+        )
         print(" done.")
 
     # Compile regexes
     rgxes = [
-        re.compile("^\s+sodipodi:docname=\"((.|\n)*?\")\n", re.M),
-        re.compile("^\s+inkscape:export=\"((.|\n)*?\")\n", re.M),
-        re.compile("^\s+inkscape:version=\"((.|\n)*?\")\n", re.M),
+        re.compile('^\s+sodipodi:docname="((.|\n)*?")\n', re.M),
+        re.compile('^\s+inkscape:export="((.|\n)*?")\n', re.M),
+        re.compile('^\s+inkscape:version="((.|\n)*?")\n', re.M),
         re.compile("^\s+<metadata\s+((.|\n)*?</metadata>)\n", re.M),
         re.compile("^\s+<defs\s+((.|\n)*?/>)\n", re.M),
-        re.compile("^\s+<sodipodi:namedview\s+((.|\n)*?/>)\n", re.M)
+        re.compile("^\s+<sodipodi:namedview\s+((.|\n)*?/>)\n", re.M),
     ]
 
     # Remove excess metadata from Inkscape and plain SVGs
